d266b8025738c8d1e1341a7f5478c18c25b85138
koolkrafter5 <creepersrgreen@gmail.com>
2026-03-04 04:19:09 -0500

    Touch up aspect combination handler

--- src/main/java/com/gtnewhorizons/aspectrecipeindex/nei/AspectCombinationHandler.java
+++ src/main/java/com/gtnewhorizons/aspectrecipeindex/nei/AspectCombinationHandler.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.StatCollector;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.lwjgl.opengl.GL11;
 
 import com.gtnewhorizons.aspectrecipeindex.ModItems;
 import com.gtnewhorizons.aspectrecipeindex.client.ARIClient;
@@ -17,6 +18,7 @@ import com.gtnewhorizons.aspectrecipeindex.util.TCUtil;
 import codechicken.lib.gui.GuiDraw;
 import codechicken.nei.PositionedStack;
 import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.research.ResearchCategories;
 import thaumcraft.common.Thaumcraft;
 
 public class AspectCombinationHandler extends TemplateThaumHandler {
@@ -27,12 +29,29 @@ public class AspectCombinationHandler extends TemplateThaumHandler {
     }
 
     @Override
+    public String getOverlayIdentifier() {
+        return "thaumcraft.aspects";
+    }
+
+    @Override
     public void loadCraftingRecipes(ItemStack result) {
         if (result.getItem() instanceof ItemAspect) {
             Aspect aspect = ItemAspect.getAspect(result);
-            if (Thaumcraft.proxy.playerKnowledge.hasDiscoveredAspect(TCUtil.username, aspect)) {
+            if (TCUtil.shouldShowAspect(aspect)) {
                 new AspectCombinationRecipe(result);
             }
+        }
+    }
+
+    @Override
+    public void loadCraftingRecipes(String inputId, Object... results) {
+        if (inputId.equals("item")) loadCraftingRecipes((ItemStack) results[0]);
+        if (!inputId.equals(this.getOverlayIdentifier())) return;
+        for (Aspect aspect : Aspect.aspects.values()) {
+            if (!TCUtil.shouldShowAspect(aspect)) continue;
+            ItemStack result = new ItemStack(ModItems.itemAspect);
+            ItemAspect.setAspect(result, aspect);
+            new AspectCombinationRecipe(result);
         }
     }
 
@@ -58,9 +77,16 @@ public class AspectCombinationHandler extends TemplateThaumHandler {
     }
 
     @Override
+    public void loadUsageRecipes(String inputId, Object... ingredients) {
+        if (inputId.equals("item")) loadUsageRecipes((ItemStack) ingredients[0]);
+        else if (inputId.equals(this.getOverlayIdentifier())) loadCraftingRecipes(inputId, ingredients);
+    }
+
+    @Override
     public void drawBackground(int recipe) {
         AspectCombinationRecipe cachedRecipe = (AspectCombinationRecipe) arecipes.get(recipe);
         if (cachedRecipe.getIngredients().isEmpty()) {
+            super.drawBackground(recipe);
             int startY = 25;
             GuiDraw.drawStringC(
                     StatCollector.translateToLocal("tc.aspect.primal"),
@@ -69,9 +95,12 @@ public class AspectCombinationHandler extends TemplateThaumHandler {
                     ariClient.getColor("aspectrecipeindex.gui.textColor"),
                     false);
         } else {
+            GL11.glTranslatef(-32F, 8, 0F);
+            super.drawBackground(recipe);
+            GL11.glTranslatef(32F, -8F, 0F);
             int spaceX = 16;
             int startX = ARIClient.NEI_GUI_WIDTH / 2 - (16 + (16 + spaceX) * 2) / 2;
-            int startY = 6;
+            int startY = 13;
             DrawUtils.drawXYCenteredString(
                     "=",
                     startX + 24,
@@ -88,29 +117,39 @@ public class AspectCombinationHandler extends TemplateThaumHandler {
     }
 
     @Override
-    public void drawForeground(int recipe) {}
+    public void drawForeground(int recipe) {
+        drawExtras(recipe);
+    }
 
-    private class AspectCombinationRecipe extends CachedRecipe {
+    @Override
+    public void drawExtras(int recipe) {
+        // Only show these for the first result to limit clutter
+        if (recipe == 0) {
+            super.drawExtras(recipe);
+        }
+    }
+
+    private class AspectCombinationRecipe extends CachedThaumRecipe {
 
         private final List<PositionedStack> ingredients = new ArrayList<>();
         private final PositionedStack result;
 
         public AspectCombinationRecipe(ItemStack aspectStack) {
+            super(true);
             arecipes.add(this);
-
-            int startY = 0;
+            prereqs.add(new ResearchInfo(ResearchCategories.getResearch("ASPECTS"), true));
 
             Aspect aspect = ItemAspect.getAspect(aspectStack);
             aspectStack = new ItemStack(ModItems.itemAspect);
             ItemAspect.setAspect(aspectStack, aspect);
 
             if (aspect.isPrimal()) {
-                this.result = new PositionedStack(aspectStack, ARIClient.NEI_GUI_WIDTH / 2 - 16 / 2, startY + 6);
+                this.result = new PositionedStack(aspectStack, ARIClient.NEI_GUI_WIDTH / 2 - 16 / 2, 5);
             } else {
                 int spaceX = 16;
                 int startX = ARIClient.NEI_GUI_WIDTH / 2 - (16 + (16 + spaceX) * 2) / 2;
 
-                this.result = new PositionedStack(aspectStack, startX, startY + 6);
+                this.result = new PositionedStack(aspectStack, startX, 13);
 
                 Aspect[] components = aspect.getComponents();
 
@@ -119,8 +158,8 @@ public class AspectCombinationHandler extends TemplateThaumHandler {
                 ItemStack secondIngred = new ItemStack(ModItems.itemAspect);
                 ItemAspect.setAspect(secondIngred, components[1]);
 
-                ingredients.add(new PositionedStack(firstIngred, startX + (spaceX + 16), startY + 6));
-                ingredients.add(new PositionedStack(secondIngred, startX + (spaceX + 16) * 2, startY + 6));
+                ingredients.add(new PositionedStack(firstIngred, startX + (spaceX + 16), 13));
+                ingredients.add(new PositionedStack(secondIngred, startX + (spaceX + 16) * 2, 13));
             }
         }
 
